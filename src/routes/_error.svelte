f64f373d9eca4778b14eba110f3f915211e6b940
Kapil <kapil.pipaliya@yahoo.com>
2019-10-16 10:06:48 +0530

    fix error page.

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -3,17 +3,7 @@
 <style src='./__error-3.less' lang='less'></style>
 <template src='./__error-4.pug'></template>
 <!-- manual_part -->
-
 <svelte:head>
 	<title>{status}</title>
 </svelte:head>
-
-<h1>{status}</h1>
-
-<p>{error.message}</p>
-
-{#if dev && error.stack}
-	<pre>{error.stack}</pre>
-{/if}
-
 <!-- manual_part_end -->
